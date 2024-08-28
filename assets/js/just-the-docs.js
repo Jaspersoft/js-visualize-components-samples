b7c22d89bc16f3a6694a3798b976abbc00b5eeac
Eduardo Canche Vazquez <ecanchev@tibco.com>
2024-08-28 11:26:04 -0600

    important note added at the end of the styling markdown

--- assets/js/just-the-docs.js
+++ assets/js/just-the-docs.js
@@ -59,7 +59,7 @@ jtd.getTheme = function() {
 
 jtd.setTheme = function(theme) {
   var cssFile = document.querySelector('[rel="stylesheet"]');
-  cssFile.setAttribute('href', '/js-visualize-components-samples/assets/css/just-the-docs-' + theme + '.css');
+  cssFile.setAttribute('href', 'http://localhost:4000/js-visualize-components-samples/assets/css/just-the-docs-' + theme + '.css');
 }
 
 // Document ready
