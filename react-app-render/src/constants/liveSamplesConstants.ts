ee248a64f77e7c7aa1d75ad887a4b52c206c3154
Grant Bacon <gbacon@tibco.com>
2024-10-15 12:52:03 -0700

    Move constants closer to method theyre used in

--- react-app-render/src/constants/liveSamplesConstants.ts
+++ react-app-render/src/constants/liveSamplesConstants.ts
@@ -1,12 +1,7 @@
-export const inputControlsPageStart = "/input-controls/live-sample.html";
-export const inputControlsTypesPageStart = "/input-controls/types-samples.html";
-export const schedulerPageStart = "/scheduler/demo.html";
-export const applicationPageStart = "Application.html";
-
 export const aceEditorModes: { [key: string]: string } = {
-    'js': 'javascript',
-    'react': 'typescript',
-    'html': 'html',
-    'css': 'css',
-    'preview': 'javascript'
-}
+  js: "javascript",
+  react: "typescript",
+  html: "html",
+  css: "css",
+  preview: "javascript",
+};
