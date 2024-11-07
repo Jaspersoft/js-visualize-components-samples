46a308c3d2a4e648b632607091f078b738a7ec6a
Eduardo Canche Vazquez <ecanchev@tibco.com>
2024-11-07 10:51:35 -0600

    fix(react-app-render): remove styles from final build.

--- react-app-render/src/index.tsx
+++ react-app-render/src/index.tsx
@@ -5,7 +5,6 @@
  */
 import {createRoot} from "react-dom/client";
 import Main from "./Main";
-import "@jaspersoft/jv-ui-components/dist/jv-ui.css";
 
 const rootEl = document.getElementById("root");
 createRoot(rootEl!).render(<Main/>)
